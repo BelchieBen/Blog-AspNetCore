86e040a3d2e60e74839b24c080d45bcf761addda
benjamin.belcher <benjamin.belcher@ideagen.com>
2021-09-17 15:02:29 +0100

    Added user sign up and tweaked UI

--- Blog/Controllers/AuthController.cs
+++ Blog/Controllers/AuthController.cs
@@ -43,7 +43,7 @@ namespace Blog.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Auth");
         }
 
         [HttpGet]
